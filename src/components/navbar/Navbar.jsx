7509c361e991834793e2cea05f3426ded32c897e
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2023-02-20 01:31:26 +0530

    add resposiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,11 @@ import PeopleIcon from '@mui/icons-material/People';
 import { useState } from "react";
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import { styled } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Navbar() {
-  const [isAtHome, setAtHome] = useState(true);
+  const [active, setActive] = useState(true);
 
   const BootstrapTooltip = styled(({ className, ...props }) => (
     <Tooltip {...props} classes={{ popper: className }} />
@@ -30,26 +32,34 @@ export default function Navbar() {
         <img src="./assets/logo.png" alt="logo" />
         <label htmlFor="search">
           <div><SearchIcon xlassName="search_icon" /></div>
-          <div><input type="text" id="search" placeholder="Search Facebook " /></div>
+          <div className="search_input_box"><input type="text" id="search" placeholder="Search Facebook " /></div>
         </label>
 
       </div>
+      
       <div className="middle_part">
         <div></div>
         <div className="mid_icons_container">
-          <Link to="/" className={isAtHome ? "active" : "inactive"} onClick={() => setAtHome(true)}>
-            <HomeIcon sx={{ fontSize: 36 }} className="icon" onClick={() => setAtHome(true)} />
+          <Link to="/" className={active ? "active" : "inactive"} onClick={() => setActive(true)}>
+            <HomeIcon sx={{ fontSize: 36 }} className="icon" onClick={() => setActive(true)} />
           </Link>
-          <Link to="/friends" className={isAtHome ? "inactive" : "active"} onClick={() => setAtHome(false)}>
-            <PeopleIcon sx={{ fontSize: 36 }} className="icon" onClick={() => setAtHome(false)} />
+          <Link to="/friends" className={active ? "inactive" : "active"} onClick={() => setActive(false)}>
+            <PeopleIcon sx={{ fontSize: 36 }} className="icon" onClick={() => setActive(false)} />
+          </Link>
+          <Link to="/bookmarks" className={active ? "inactive hamburger_icon" : "active hamburger_icon"} onClick={() => setActive(false)}>
+            <MenuIcon sx={{ fontSize: 36 }} className="icon" onClick={() => setActive(false)} />
           </Link>
         </div>
         <div></div>
       </div>
+
       <div className="last_part">
         <Link to="/friends" className="find_friends">Find Friends</Link>
         <div className="menu">
           <BootstrapTooltip title="Menu" placement="bottom" ><img src="./assets/menu.png" alt="menu" /></BootstrapTooltip>
+        </div>
+        <div className="add">
+          <BootstrapTooltip title="Create" placement="bottom" ><AddIcon /></BootstrapTooltip>
         </div>
         <div className="messenger_logo">
           <BootstrapTooltip title="Messenger" placement="bottom"><img src="./assets/messenger.png" alt="messenger logo" /></BootstrapTooltip>
@@ -60,7 +70,6 @@ export default function Navbar() {
         <div className="profile">
           <BootstrapTooltip title="Account" placement="bottom" ><img src="./assets/person/person10.jpg" alt="account" /></BootstrapTooltip>
         </div>
-
       </div>
 
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,20 @@ import Leftbar from '../../components/leftbar/Leftbar'
 import Navbar from '../../components/navbar/Navbar'
 import Feed from '../../components/feed/Feed'
 import Rightbar from '../../components/rightbar/Rightbar'
+import { useMediaQuery } from "react-responsive"
 
 export default function Home() {
+
+  const laptopOrDesktop = useMediaQuery({query: '(min-width: 1200px)'})
+  const tablet = useMediaQuery({query: '(min-width: 960px)'});
+
   return (
     <div className='home_page_container'>
       <Navbar />
       <div className="home_page_body">
-        <Leftbar />
+        {laptopOrDesktop && <Leftbar />}
         <Feed />
-        <Rightbar />
+        {tablet && <Rightbar />}
       </div>
     </div>
   )
